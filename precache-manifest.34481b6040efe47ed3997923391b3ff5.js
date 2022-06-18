e2dec6a9d479955c34d493f2db06d724c5a941ad
Nature <874572491@qq.com>
2022-06-18 17:06:52 +0800

    deploy

---
+++ precache-manifest.34481b6040efe47ed3997923391b3ff5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c0690acd9f5ff715f8",
+    "url": "/mapleleavesnotepage/css/app.888410ca.css"
+  },
+  {
+    "revision": "8fb1c5c2cf14af75d030",
+    "url": "/mapleleavesnotepage/css/chunk-17598f51.b6d518e4.css"
+  },
+  {
+    "revision": "5525a3a51083fcfea882",
+    "url": "/mapleleavesnotepage/css/chunk-522d83b1.e61fd716.css"
+  },
+  {
+    "revision": "128d820a17dc860e6c49",
+    "url": "/mapleleavesnotepage/css/chunk-780b40b5.2a4def60.css"
+  },
+  {
+    "revision": "09024295af4819263432",
+    "url": "/mapleleavesnotepage/css/chunk-7a696e6d.b299ba0b.css"
+  },
+  {
+    "revision": "968e5b3937432c2a64a0",
+    "url": "/mapleleavesnotepage/css/chunk-a865e226.be1b5421.css"
+  },
+  {
+    "revision": "162c20c50fc7066ad037",
+    "url": "/mapleleavesnotepage/css/chunk-vendors.9181e156.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/mapleleavesnotepage/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/mapleleavesnotepage/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "3bbf9d74bc2da8104064a3367cc0486a",
+    "url": "/mapleleavesnotepage/index.html"
+  },
+  {
+    "revision": "53c0690acd9f5ff715f8",
+    "url": "/mapleleavesnotepage/js/app.f8f0190f.js"
+  },
+  {
+    "revision": "8fb1c5c2cf14af75d030",
+    "url": "/mapleleavesnotepage/js/chunk-17598f51.2120cf9d.js"
+  },
+  {
+    "revision": "5525a3a51083fcfea882",
+    "url": "/mapleleavesnotepage/js/chunk-522d83b1.530eb217.js"
+  },
+  {
+    "revision": "128d820a17dc860e6c49",
+    "url": "/mapleleavesnotepage/js/chunk-780b40b5.d1a75c08.js"
+  },
+  {
+    "revision": "09024295af4819263432",
+    "url": "/mapleleavesnotepage/js/chunk-7a696e6d.d576cc2a.js"
+  },
+  {
+    "revision": "968e5b3937432c2a64a0",
+    "url": "/mapleleavesnotepage/js/chunk-a865e226.33f513a5.js"
+  },
+  {
+    "revision": "162c20c50fc7066ad037",
+    "url": "/mapleleavesnotepage/js/chunk-vendors.aa0e4d73.js"
+  },
+  {
+    "revision": "6b639dd683f371c8c5a82bcd9238b784",
+    "url": "/mapleleavesnotepage/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mapleleavesnotepage/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mapleleavesnotepage/precache-manifest.34481b6040efe47ed3997923391b3ff5.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "mapleleavesnote"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
